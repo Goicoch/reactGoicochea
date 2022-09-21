81f5fdffcdb1daf4ea28e68a60f2bdd5609efd49
Iñaki Goicochea <goicoina@gmail,com>
2022-09-20 22:29:38 -0300

    logoNavBar

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div>
-            <h1>hola</h1>
+            <img src="/favicon.ico"></img>
         </div>
          );
         }
