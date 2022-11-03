08e73f2f50fab1a525de035c5d32d27a98ae00fc
Iñaki Goicochea <goicoina@gmail,com>
2022-11-03 12:18:14 -0300

    .

--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -3,20 +3,19 @@ import "./ItemCart.css";
 import {useCartContext} from "..//../Context/CartProvider"
 
 const ItemCart = ({books}) =>{
-
 const {removeBook} = useCartContext();
 
     return(
         <>
-<div className="itemCart">
-<img src={books.image} alt={books.name} />
-<div>
-    <p>Titulo: {books.name}</p>
-    <p>Cantidad: {books.cantidad}</p>
-    <p>Precio:$ {books.price} </p>
-    <p>Sub total:${books.cantidad * books.price}</p>
-    <button onClick={() => removeBook (books.id)} className="btn btn-outline-dark">Eliminar </button>
-</div>
+        <div className="itemCart">
+        <img src={books.image} alt={books.name} />
+        <div>
+            <p>Titulo: {books.name}</p>
+            <p>Cantidad: {books.cantidad}</p>
+            <p>Precio:$ {books.price} </p>
+            <p>Sub total:${books.cantidad * books.price}</p>
+            <button onClick={() => removeBook (books.id)} className="btn btn-outline-dark">Eliminar </button>
+        </div>
 
 </div>
 </>
